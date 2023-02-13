e5ab0b53b446f0c1c19fb9f64af1692f1251c2b0
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2023-02-13 19:17:58 +0100

    Revert "Login/register"

--- src/js/api/constants.mjs
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = "https://api.noroff.dev/api/v1";
-export const API_AUCTION = "/auction";
-export const API_URL = `${API_BASE_URL}${API_AUCTION}`;
--- src/js/index.mjs
+++
@@ -1,9 +0,0 @@
-import * as listeners from "./handlers/index.mjs";
-
-const path = location.pathname;
-
-if (path === "/pages/login/") {
-  listeners.setLoginFormListener();
-} else if (path === "/pages/register/") {
-  listeners.setRegisterFormListener();
-}
